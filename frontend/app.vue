982a918ac95d5d63ae3a37f0d4a995e5e7cb9f0c
Andy @ Personal <andywillekens@gmail.com>
2025-03-21 15:03:43 +0100

    Styling

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
-const { data } = await useFetch(`/api/stats`)
+  const { data } = await useFetch(`/api/stats`)
 
-// Process the data: group by user and count occurrences
-const groupedData = computed(() => {
-  if (!data.value) return [];
+  // Process the data: group by user and count occurrences
+  const groupedData = computed(() => {
+    if (!data.value) return []
 
-  // Group by user and count occurrences
-  const userCounts = data.value.reduce((acc, item) => {
-    acc[item.user] = (acc[item.user] || 0) + 1;
-    return acc;
-  }, {} as Record<string, number>);
+    // Group by user and count occurrences
+    const userCounts = data.value.reduce((acc, item) => {
+      acc[item.user] = (acc[item.user] || 0) + 1
+      return acc
+    }, {} as Record<string, number>)
 
-  // Convert object to sorted array
-  return Object.entries(userCounts)
-    .map(([user, total]) => ({ user, total }))
-    .sort((a, b) => b.total - a.total);
-});
+    // Convert object to sorted array
+    return Object.entries(userCounts)
+      .map(([user, total]) => ({ user, total }))
+      .sort((a, b) => b.total - a.total)
+  })
 </script>
 
 <template>
-  <ul>
-    <li v-for="item in groupedData" :key="item.user">
-      {{ item.user }}: {{ item.total }}
-    </li>
-  </ul>
+  <section class="w-[600px] flex flex-col gap-6">
+    <h1>🏆 Koffie leaderboard:</h1>
+    <ul>
+      <li v-for="item in groupedData" :key="item.user">{{ item.user }}: {{ item.total }}</li>
+    </ul>
+  </section>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  css: ['~/assets/css/main.css'],
   runtimeConfig: {
     apiUrl: process.env.API_URL,
     bearerToken: process.env.BEARER_TOKEN
   },
-  modules: [
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/fonts',
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/fonts']
 })
